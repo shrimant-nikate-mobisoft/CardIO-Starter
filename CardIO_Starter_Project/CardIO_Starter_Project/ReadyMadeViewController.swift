9945a5e115f5ec186d18b95f85b933e520d82540
Shrimant <shrimant.nikate@mobisoftinfotech.com>
2016-04-26 15:56:25 +0530

    added git ignore files

--- CardIO_Starter_Project/CardIO_Starter_Project/ReadyMadeViewController.swift
+++ CardIO_Starter_Project/CardIO_Starter_Project/ReadyMadeViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ReadyMadeViewController: UIViewController {
 
+    @IBOutlet weak var labelResults: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
